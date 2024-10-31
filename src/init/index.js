daa9f798596fdc8d1eaf832a2fa09db8a5e4fb0a
hanwj75 <wjws9608@gmail.com>
2024-10-31 16:46:51 +0900

    Add : "initServer추가"
    서버 실행전 불러올 데이터를 호출하는 함수

---
+++ src/init/index.js
@@ -0,0 +1,5 @@
+//서버 실행시 불러올 데이터를 여기 추가
+
+const initServer = async () => {};
+
+export default initServer;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import net from "net";
 import { HOST, PORT } from "./constants/env.js";
 import { onConnection } from "./events/onConnection.js";
+import initServer from "./init/index.js";
 
 const server = net.createServer(onConnection);
 
-server.listen(() => {
-  console.log(`서버 켜짐 ${PORT} : ${HOST}`);
-});
+//서버실행시 필요한 데이터들이 전부 들어왔다면 그 이후 서버를 실행한다.
+
+initServer()
+  .then(() => {
+    server.listen(() => {
+      console.log(`서버 켜짐 ${PORT} : ${HOST}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
