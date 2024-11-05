57a330c72e59f14e9778399cfc61d0f4af4bfdd3
hanwj75 <wjws9608@gmail.com>
2024-11-05 16:18:04 +0900

    Update : "환경변수"

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 //환경 설정과 관련된 모든것
 
-import { DB1_HOST, DB1_NAME, DB1_PASSWORD, DB1_PORT, DB1_USER } from "../constants/env.js";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "../constants/env.js";
 //설정 객체 정의
 export const config = {
   //db설정
   database: {
-    database: DB1_NAME,
-    user: DB1_USER,
-    password: DB1_PASSWORD,
-    host: DB1_HOST,
-    port: DB1_PORT,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
   },
 };
